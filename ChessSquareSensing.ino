32909f818caadad3a680575c18ec70ef48f30919
sezargari <42723695+sezargari@users.noreply.github.com>
2021-04-17 01:23:23 -0500

    File to test pin to square connections
    Outputs the pin the square is connected to when a piece is placed on it

---
+++ ChessSquareSensing.ino
@@ -0,0 +1,283 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+ 
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+  pinMode(15, INPUT_PULLUP);
+  pinMode(16, INPUT_PULLUP);
+  pinMode(17, INPUT_PULLUP);
+  
+  pinMode(18, INPUT_PULLUP);
+  pinMode(19, INPUT_PULLUP);
+  pinMode(20, INPUT_PULLUP);
+  pinMode(21, INPUT_PULLUP);
+  pinMode(22, INPUT_PULLUP);
+  pinMode(23, INPUT_PULLUP);
+  pinMode(24, INPUT_PULLUP);
+  pinMode(25, INPUT_PULLUP);
+  pinMode(26, INPUT_PULLUP);
+  pinMode(27, INPUT_PULLUP);
+  pinMode(28, INPUT_PULLUP);
+  pinMode(29, INPUT_PULLUP);
+  pinMode(30, INPUT_PULLUP);
+  pinMode(31, INPUT_PULLUP);
+  pinMode(32, INPUT_PULLUP);
+  pinMode(33, INPUT_PULLUP);
+
+  pinMode(34, INPUT_PULLUP);
+  pinMode(35, INPUT_PULLUP);
+  pinMode(36, INPUT_PULLUP);
+  pinMode(37, INPUT_PULLUP);
+  pinMode(38, INPUT_PULLUP);
+  pinMode(39, INPUT_PULLUP);
+  pinMode(40, INPUT_PULLUP);
+  pinMode(41, INPUT_PULLUP);
+  pinMode(42, INPUT_PULLUP);
+  pinMode(43, INPUT_PULLUP);
+  pinMode(44, INPUT_PULLUP);
+  pinMode(45, INPUT_PULLUP);
+  pinMode(46, INPUT_PULLUP);
+  pinMode(47, INPUT_PULLUP);
+  pinMode(48, INPUT_PULLUP);
+  pinMode(49, INPUT_PULLUP);
+
+  pinMode(50, INPUT_PULLUP);
+  pinMode(51, INPUT_PULLUP);
+  pinMode(52, INPUT_PULLUP);
+  pinMode(53, INPUT_PULLUP);
+  pinMode(A15, INPUT_PULLUP);
+  pinMode(A14, INPUT_PULLUP);
+  pinMode(A13, INPUT_PULLUP);
+  pinMode(A12, INPUT_PULLUP);
+  pinMode(A11, INPUT_PULLUP);
+  pinMode(A10, INPUT_PULLUP);
+  pinMode(A9, INPUT_PULLUP);
+  pinMode(A8, INPUT_PULLUP);
+  pinMode(A7, INPUT_PULLUP);
+  pinMode(A6, INPUT_PULLUP);
+  pinMode(A5, INPUT_PULLUP);
+  pinMode(A4, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+  if (!digitalRead(2)) {
+    Serial.println("Pin 2");
+  }
+  if (!digitalRead(3)) {
+    Serial.println("Pin 3");
+  }
+  if (!digitalRead(4)) {
+    Serial.println("Pin 4");
+  }
+  if (!digitalRead(5)) {
+    Serial.println("Pin 5");
+  }
+  if (!digitalRead(6)) {
+    Serial.println("Pin 6");
+  }
+  if (!digitalRead(7)) {
+    Serial.println("Pin 7");
+  }
+  if (!digitalRead(8)) {
+    Serial.println("Pin 8");
+  }
+  if (!digitalRead(9)) {
+    Serial.println("Pin 9");
+  }
+  if (!digitalRead(10)) {
+    Serial.println("Pin 10");
+  }
+  if (!digitalRead(11)) {
+    Serial.println("Pin 11");
+  }
+  if (!digitalRead(12)) {
+    Serial.println("Pin 12");
+  }
+  if (!digitalRead(13)) {
+    Serial.println("Pin 13");
+  }
+  if (!digitalRead(14)) {
+    Serial.println("Pin 14");
+  }
+  if (!digitalRead(15)) {
+    Serial.println("Pin 15");
+  }
+  if (!digitalRead(16)) {
+    Serial.println("Pin 16");
+  }
+  if (!digitalRead(17)) {
+    Serial.println("Pin 17");
+  }
+
+  
+  if (!digitalRead(18)) {
+    Serial.println("Pin 18");
+  }
+  if (!digitalRead(19)) {
+    Serial.println("Pin 19");
+  }
+  if (!digitalRead(20)) {
+    Serial.println("Pin 20");
+  }
+  if (!digitalRead(21)) {
+    Serial.println("Pin 21");
+  }
+  if (!digitalRead(22)) {
+    Serial.println("Pin 22");
+  }
+  if (!digitalRead(23)) {
+    Serial.println("Pin 23");
+  }
+  if (!digitalRead(24)) {
+    Serial.println("Pin 24");
+  }
+  if (!digitalRead(25)) {
+    Serial.println("Pin 25");
+  }
+  if (!digitalRead(26)) {
+    Serial.println("Pin 26");
+  }
+  if (!digitalRead(27)) {
+    Serial.println("Pin 27");
+  }
+  if (!digitalRead(28)) {
+    Serial.println("Pin 28");
+  }
+  if (!digitalRead(29)) {
+    Serial.println("Pin 29");
+  }
+  if (!digitalRead(30)) {
+    Serial.println("Pin 30");
+  }
+  if (!digitalRead(31)) {
+    Serial.println("Pin 31");
+  }
+  if (!digitalRead(32)) {
+    Serial.println("Pin 32");
+  }
+  if (!digitalRead(33)) {
+    Serial.println("Pin 33");
+  }
+
+
+
+
+  if (!digitalRead(34)) {
+    Serial.println("Pin 34");
+  }
+  if (!digitalRead(35)) {
+    Serial.println("Pin 35");
+  }
+  if (!digitalRead(36)) {
+    Serial.println("Pin 36");
+  }
+  if (!digitalRead(37)) {
+    Serial.println("Pin 37");
+  }
+  if (!digitalRead(38)) {
+    Serial.println("Pin 38");
+  }
+  if (!digitalRead(39)) {
+    Serial.println("Pin 39");
+  }
+  if (!digitalRead(40)) {
+    Serial.println("Pin 40");
+  }
+  if (!digitalRead(41)) {
+    Serial.println("Pin 41");
+  }
+  if (!digitalRead(42)) {
+    Serial.println("Pin 42");
+  }
+  if (!digitalRead(43)) {
+    Serial.println("Pin 43");
+  }
+  if (!digitalRead(44)) {
+    Serial.println("Pin 44");
+  }
+  if (!digitalRead(45)) {
+    Serial.println("Pin 45");
+  }
+  if (!digitalRead(46)) {
+    Serial.println("Pin 46");
+  }
+  if (!digitalRead(47)) {
+    Serial.println("Pin 47");
+  }
+  if (!digitalRead(48)) {
+    Serial.println("Pin 48");
+  }
+  if (!digitalRead(49)) {
+    Serial.println("Pin 49");
+  }
+
+  
+  if (!digitalRead(50)) {
+    Serial.println("Pin 50");
+  }
+  if (!digitalRead(51)) {
+    Serial.println("Pin 51");
+  }
+  if (!digitalRead(52)) {
+    Serial.println("Pin 52");
+  }
+  if (!digitalRead(53)) {
+    Serial.println("Pin 53");
+  }
+  if (!digitalRead(A15)) {
+    Serial.println("Pin A15");
+  }
+  if (!digitalRead(A14)) {
+    Serial.println("Pin A14");
+  }
+  if (!digitalRead(A13)) {
+    Serial.println("Pin A13");
+  }
+  if (!digitalRead(A12)) {
+    Serial.println("Pin A12");
+  }
+  if (!digitalRead(A11)) {
+    Serial.println("Pin A11");
+  }
+  if (!digitalRead(A10)) {
+    Serial.println("Pin A10");
+  }
+  if (!digitalRead(A9)) {
+    Serial.println("Pin A9");
+  }
+  if (!digitalRead(A8)) {
+    Serial.println("Pin A8");
+  }
+  if (!digitalRead(A7)) {
+    Serial.println("Pin A7");
+  }
+  if (!digitalRead(A6)) {
+    Serial.println("Pin A6");
+  }
+  if (!digitalRead(A5)) {
+    Serial.println("Pin A5");
+  }
+  if (!digitalRead(A4)) {
+    Serial.println("Pin A4");
+  }
+
+
+
+  
+  delay(500);
+}
